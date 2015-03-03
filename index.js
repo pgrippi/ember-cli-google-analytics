bcb31f5dc6d9726f9cef7f669bec61f5bf880a81
Raymond Cox <raymond@helios.raymondjcox.com>
2015-03-03 10:39:50 -0500

    Don't use protocol relative URL's
    // on a device resolves to file://

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ function analyticsTrackingCode(config) {
     "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){",
     "(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),",
     "m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)",
-    "})(window,document,'script','//www.google-analytics.com/analytics.js','" + config.globalVariable + "');",
+    "})(window,document,'script','https://www.google-analytics.com/analytics.js','" + config.globalVariable + "');",
     "",
     "" + config.globalVariable + "('create', '" + config.webPropertyId + "', " + gaConfig + ");",
     "</script>"
