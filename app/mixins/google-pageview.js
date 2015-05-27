8ac3f9cd30f01d826d5808421a350c704f2f78f7
Gastón I. Silva <givanse@gmail.com>
2015-05-27 12:20:33 -0500

    Call manually pageviewToGA()

--- app/mixins/google-pageview.js
+++ app/mixins/google-pageview.js
@@ -2,7 +2,11 @@ import Ember from 'ember';
 import ENV from '../config/environment';
 
 export default Ember.Mixin.create({
-  pageviewToGA: function() {
+
+  pageviewToGA: function(page, title) {
+    var page = page ? page : this.get('url');
+    var title = title ? title : this.get('url');
+
     if (Ember.get(ENV, 'googleAnalytics.webPropertyId') != null) {
       var trackerType = Ember.getWithDefault(ENV, 'googleAnalytics.tracker', 'analytics.js');
 
@@ -10,12 +14,13 @@ export default Ember.Mixin.create({
         var globalVariable = Ember.getWithDefault(ENV, 'googleAnalytics.globalVariable', 'ga');
 
         window[globalVariable]('send', 'pageview', {
-          page: this.get('url'),
-          title: this.get('url')
+          page: page,
+          title: title 
         });
       } else if (trackerType === 'ga.js') {
         window._gaq.push(['_trackPageview']);
       }
     }
   }.on('didTransition')
+
 });
